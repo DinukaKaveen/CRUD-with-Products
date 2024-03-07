dd7af04bea3912d7fb390ccbd8c04ba57f71112e
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-03-07 22:02:14 +0530

    Update Home.js

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -32,7 +32,7 @@ function Home() {
               type="checkbox"
               value=""
               className="sr-only peer"
-              defaultChecked
+              defaultChecked={row.activeStatus === "Active"}
             />
             <div className="relative w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
           </label>
